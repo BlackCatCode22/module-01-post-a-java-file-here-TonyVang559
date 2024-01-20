6598ca7d51b6b613e011e8c2b0b20fd16af2d83b
Tony Vang <97489898+TonyVang559@users.noreply.github.com>
2024-01-19 19:02:06 -0800

    Add files via upload

---
+++ if_statement.java
@@ -0,0 +1,14 @@
+public class if_statement {
+    public static void main(String[] args) {
+
+        int myAge = 33;
+
+        if (myAge > 25) {
+
+            System.out.println(" My age is greater than 25");
+
+        }
+
+    }
+
+}
